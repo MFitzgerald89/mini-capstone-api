a05f39a36b2c373ae324ebcfdbcc1149c38be047
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-02-28 21:19:44 -0500

    added Catback Exhaust seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+product = Product.new({name: "Catback Exhaust", price: 1200, image_url: "https://www.parkmuffler.com/wp-content/uploads/2018/08/catback-exhaust.jpg", description: "part of the exhaust system that's behind the catalytic converter"})
+
