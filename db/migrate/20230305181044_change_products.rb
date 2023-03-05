3bebd48a8b052522e54fb73778245a72058e5ba5
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-03-05 13:20:54 -0500

    adds in new migrations to products table, adds seed_dump to gemfile

---
+++ db/migrate/20230305181044_change_products.rb
@@ -0,0 +1,8 @@
+class ChangeProducts < ActiveRecord::Migration[7.0]
+  def change
+    change_column :products, :description, :text
+
+    change_column :products, :price, :integer, precision: 9, scale: 2
+    add_column :products, :inventory, :integer
+  end
+end
