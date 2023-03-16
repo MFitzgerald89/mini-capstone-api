2e3e10c213a054f6674b193282f9052b73fa1df9
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-03-15 22:25:54 -0400

    added has_many, through associations in product and category models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,5 @@
 class Category < ApplicationRecord
   has_many :category_products
-
-  def products
-    category_products.map do |category_product|
-      category_product.product
-    end
-
-  end
+  has_many :products, through :category_products
+  
 end
